eec579a4b6bfacde437aebc16e95184047956c07
greaterthanstar <greaterthanstar@sent.com>
2026-05-14 14:12:50 -0700

    The container is failing because Node ESM cannot resolve a relative import without a file extension.

--- packages/shared/src/types/index.ts
+++ packages/shared/src/types/index.ts
@@ -3,6 +3,6 @@
  * Export all type utilities from this central location
  */
 
-export * from './errors';
-export * from './logger';
-export * from './utils';
+export * from './errors.js';
+export * from './logger.js';
+export * from './utils.js';
